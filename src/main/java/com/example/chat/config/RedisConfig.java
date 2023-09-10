e62018eebba5158ff410e1ff3d5ff4ec2b8d376e
Sangmin <101931428+netco97@users.noreply.github.com>
2023-09-10 16:21:46 +0900

    redis pub/sub 테스트 및 멀티 커넥팅

--- src/main/java/com/example/chat/config/RedisConfig.java
+++ src/main/java/com/example/chat/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.example.chat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
---
+++ src/main/java/com/example/chat/config/ServletConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.chat.config;
+
+import java.util.Collections;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //다중포트 띄우는 config
+public class ServletConfiguration {
+
+    @Value("${server.port.http}")
+    private int serverPortHttp;
+
+    @Bean
+    public ServletWebServerFactory serverFactory(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+    private Connector createStandardConnector(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(serverPortHttp);
+        return connector;
+    }
+}
