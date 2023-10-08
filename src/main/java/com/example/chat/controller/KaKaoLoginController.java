f0587c1f024aaad31aed1aa9c9a4631a56b139dd
Sangmin <101931428+netco97@users.noreply.github.com>
2023-10-08 15:04:33 +0900

    login 수정

--- src/main/java/com/example/chat/controller/KaKaoLoginController.java
+++ src/main/java/com/example/chat/controller/KaKaoLoginController.java
@@ -1,56 +1,37 @@
 package com.example.chat.controller;
 
-import com.example.chat.service.GetUserInfoService;
-import com.example.chat.service.RestJsonService;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.example.chat.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+
 @Controller
-@RequestMapping("/oauth")
+@RequiredArgsConstructor
+@RequestMapping(value = "/login")
 public class KaKaoLoginController {
 
-    @GetMapping("/kakao")
-    public String receiveAC(@RequestParam("code") String code, Model model){
-        System.out.println(code);
-        RestJsonService restJsonService = new RestJsonService();
-
-        //access_token이 포함된 JSON String을 받아온다.
-        String accessTokenJsonData = restJsonService.getAccessTokenJsonData(code);
-        if(accessTokenJsonData=="error") return "/chat/error";
-
-
-        //JSON String -> JSON Object
-        JSONObject accessTokenJsonObject = new JSONObject(accessTokenJsonData);
-
-        //access_token 추출
-        String accessToken = accessTokenJsonObject.get("access_token").toString();
-
-        //유저 정보가 포함된 JSON String을 받아온다.
-        GetUserInfoService getUserInfoService = new GetUserInfoService();
-        String userInfo = getUserInfoService.getUserInfo(accessToken);
-
-        //JSON String -> JSON Object
-        JSONObject userInfoJsonObject = new JSONObject(userInfo);
-
-        //유저의 Email 추출
-        JSONObject kakaoAccountJsonObject = (JSONObject)userInfoJsonObject.get("kakao_account");
-        System.out.println(kakaoAccountJsonObject);
-        JSONObject profile = (JSONObject) kakaoAccountJsonObject.get("profile");
-        String email = profile.get("nickname").toString();
-
-
-
-        //View에서 사용할 변수 설정
-        model.addAttribute("email", email);
-
-        System.out.println(email);
-
-        return "/chat/room";
+    private final MemberService ms;
+    @GetMapping("")
+    public String login(){
+        return "/login";
     }
 
+    @GetMapping("/kakao")
+    public String kakaoLogin(@RequestParam(value = "code", required = false)String code){
+        System.out.println("###########"+code);
+
+        String access_Token = ms.getAccessToken(code);
+
+        HashMap<String, Object> userInfo = ms.getUserInfo(access_Token);
+        System.out.println("###access_Token### : " + access_Token);
+        System.out.println("###nickname#### : " + userInfo.get("nickname"));
+        System.out.println("###email#### : " + userInfo.get("email"));
+
+        return "/test";
+
+    }
 }
