c06ef38f8e719926efa270fa9779ae2f0aac0ac3
Sangmin <101931428+netco97@users.noreply.github.com>
2023-11-08 17:45:30 +0900

    메세지 순서 조정

--- src/main/java/com/example/chat/controller/ChatController.java
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -28,7 +28,9 @@ public class ChatController {
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
         if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+        {
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        }
 
         messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
 
